11fe2828a8bbe8a0dacddb53cefb10dbf68d4081
Anshika Singh <114602538+anshika1506@users.noreply.github.com>
2026-02-06 00:45:21 +0530

    Update API endpoints for playlist calculations

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'https://youtubecalc-1.onrender.com/',
+    baseURL: 'https://youtubecalc-main.onrender.com',
 });
 
-export const calculatePlaylist = (playlistUrl) => API.post('/calculate', { playlistUrl });
-export const getHistory = () => API.get('/history');
+export const calculatePlaylist = (playlistUrl) => API.post('/api/playlist/calculate', { playlistUrl });
+export const getHistory = () => API.get('/api/playlist/history');
+
